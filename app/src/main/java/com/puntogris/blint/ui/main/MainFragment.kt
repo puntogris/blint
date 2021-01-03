69e453644bc552d85571eec745609928f24833d8
Joaco <rosariodota3@gmail.com>
2021-01-03 00:37:33 -0300

    from android studio

--- app/src/main/java/com/puntogris/blint/ui/main/MainFragment.kt
+++ app/src/main/java/com/puntogris/blint/ui/main/MainFragment.kt
@@ -31,7 +31,7 @@ class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
     }
 
     private fun createNewBusiness(){
-        println("ee")
+        println("ee22")
         binding.button2.show()
         binding.textView4.show()
         binding.button2.setOnClickListener {
