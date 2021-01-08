f72cf7011697313851e8e3a567bed3ce76828942
Joaco <rosariodota3@gmail.com>
2021-01-08 19:44:56 -0300

    fixed buttomiconsarrows not displaying correct icon

--- app/src/main/java/com/puntogris/blint/ui/custom_views/CustomButtonIconArrows.kt
+++ app/src/main/java/com/puntogris/blint/ui/custom_views/CustomButtonIconArrows.kt
@@ -1,6 +1,7 @@
 package com.puntogris.blint.ui.custom_views
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import androidx.core.content.res.ResourcesCompat
 import com.google.android.material.button.MaterialButton
@@ -11,11 +12,32 @@ class CustomButtonIconArrows(context: Context, attributeSet: AttributeSet): Mate
     private var clicked = false
 
     fun toggleIcon(){
-        icon = if (!clicked){
+        clicked = !clicked
+        setNewIcon(clicked)
+    }
+
+    private fun setNewIcon(clicked:Boolean){
+        icon = if (clicked){
             ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_keyboard_arrow_up_24, null)
         }else{
             ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_keyboard_arrow_down_24, null)
         }
-        clicked = !clicked
     }
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attributeSet,
+            R.styleable.CustomButtonIconArrows,
+            0, 0).apply {
+
+            try {
+                val startOpen = getBoolean(R.styleable.CustomButtonIconArrows_startOpen, false)
+                clicked = startOpen
+                setNewIcon(startOpen)
+            } finally {
+                recycle()
+            }
+        }
+    }
+
 }
